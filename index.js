1ff8ad758407e918036dd4e8535699629482581d
Jack Tsai <wa0327@gmail.com>
2018-05-18 13:25:09 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+import * as a from './lib/polyfill.console';
+import * as b from './lib/polyfill.setPrototypeOf';
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  input: 'index.js',
+  output: {
+    name: 'polyfills-ie9',
+    file: 'bundle/polyfills-ie9.umd.js',
+    format: 'umd'
+  }
+};
